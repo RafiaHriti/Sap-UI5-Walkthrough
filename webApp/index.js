00727f4cd49122cf527eddb561e1b2e7d2f2e007
RafiaHriti <rafianazhat@gmail.com>
2023-01-02 00:12:19 +0600

    Step 3 completed

---
+++ webApp/index.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/m/Text"
+    
+], function(Text) {
+    'use strict';
+
+     new Text(
+        {
+            text: "Hello UI5"
+        }
+     ).placeAt("content");
+    
+});
